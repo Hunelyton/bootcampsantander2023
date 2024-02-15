bee702e2b3c18ff8b9c12bd90cab5ed2521e817b
Hunelyton <hunelyton@hotmail.com>
2024-02-14 22:03:05 -0300

    first commit

---
+++ javascript/aula3_variáveis/script.js
@@ -0,0 +1,77 @@
+// tipos primitivos
+
+//bollean recebem valores verdadeiro ou falso
+/*var vOuF = false;
+console.log(typeof (vOuF));
+
+
+//number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+//String ou traduzido caractere
+var nome = "Tom";
+console.log(typeof(nome));*/
+
+
+//como declarar variável
+/*var variavel = "Tom";
+variavel = "Mendes";
+console.log(variavel);*/
+
+/*let variavel2 = 'Tom';
+variavel2 = 'Mendes'
+console.log(variavel2);*/
+
+/*const constante = 'Tom';
+constante = 'Mendes';
+console.log(constante);*/
+
+//Escopo
+/*
+Global= quando a variável é declarada fora de qualquer bloco, sua visibilidade fica
+disponível em todo o código.
+
+Local= quando a variável é declarada dentro de um bloco, sua visibilidade pode 
+ficar disponível ou não.
+
+
+var escopoGlobal ='global';
+console.log(escopoGlobal);
+
+function escopoGlobal(){
+    let escopoLocalInterno ='local';
+    console.log(escopoLocalInterno);
+}
+escopoLocal()
+
+
+toda variável precisa ser adicionada no inicio do código
+nunca utilize palavras reservadas para adicionar no código
+sempre utilizar a regra camelCase ou _
+*/
+
+/* 
+o sinal '=' no JS não significa igual e sim atribuiçãoabaixo temos o exemplo
+
+atribuicao
+var atribuicao ='Tom';
+console.log('atribuicao');
+
+
+
+var comparacao == '0' == 0;
+console.log(comparacao);
+
+Operadores aritméticos
+*/
+
+var adicao = 1+1;
+console.log(adicao);
+
+
+
+
+
+
+
